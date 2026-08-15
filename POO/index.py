3d0dd6ae97fa51b3134da540b39d71f668cdc621
jaojogadez <jao07oliveiragames@gmail.com>
2026-08-15 12:16:51 -0300

    feat: iniciando desafio

---
+++ POO/index.py
@@ -0,0 +1,13 @@
+# Ativiade diagnóstica
+titulo = "DESAFIO DE PROGRAMAÇÃO - TECH FOOD"
+print(f'\n\n{"="*50}\n{titulo.center(50)}\n{"="*50}\n\n')
+
+# OBJETIVO: Desenvolver uma operação de vendas de uma barraca.
+
+cardapio = [
+    {"nome": "Hambúrguer", "preco": 18, "estoque": 20},
+    {"nome": "Batata Frita", "preco": 10, "estoque": 25},
+    {"nome": "Refrigerante", "preco": 6, "estoque": 30},
+    {"nome": "Suco", "preco": 8, "estoque": 20},
+    {"nome": "Combo TechFood", "preco": 30, "estoque": 15}
+]
---
+++ POO/quotes.md
@@ -0,0 +1,21 @@
+
+# Quais dados o programa precisa manter?
+## Cardápio
+O que preciso saber sobre um produto?
+- código
+- nome do produto
+- estoque
+- preço
+
+## Pedido atual
+O que preciso saber enquanto estou montando um pedido?
+- nome do cliente
+- produtos
+- quantidades de produtos
+- valor final
+- descontos
+
+
+## Histórico
+O que preciso guardar depois que uma venda é confirmada?
+- O pedido (nome, produtos, qtdes, valor, desconto)
